72cc29b06c7d885b15a91af3d6ccd509b0ccefcd
Sandeep Abeykoon <sandeepchanura@gmail.com>
2023-07-16 16:47:06 +0530

    updated the code

--- Classes/Consultation.java
+++ Classes/Consultation.java
@@ -3,13 +3,36 @@ package Classes;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Date;
 
-public class Consultation {
+public class Consultation{
     private static final ArrayList<Consultation> CONSULTATION_ARRAY = new ArrayList<>();
     private LocalDate date;
-    private LocalTime time;
+    private LocalTime startTime;
+    private LocalTime endTime;
     private double cost;
     private String notes;
+    private Patient patient;
+    private Doctor doctor;
+
+    public Doctor getDoctor(){
+        return this.doctor;
+    }
+
+    public LocalDate getDate(){
+        return this.date;
+    }
+
+    public LocalTime getStartTime(){
+        return this.startTime;
+    }
+
+    public LocalTime getEndTime(){
+        return this.endTime;
+    }
+    public static ArrayList<Consultation> getConsultations(){
+        return CONSULTATION_ARRAY;
+    }
 }
 
 
--- SubFunctionalities/GUI/consultationFunctionalities.java
+++ SubFunctionalities/GUI/consultationFunctionalities.java
@@ -1,4 +1,21 @@
 package SubFunctionalities.GUI;
 
-public class consultationFunctionalities {
+import Classes.Consultation;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public abstract class consultationFunctionalities {
+    public static boolean getDoctorAvailability(String doctorId, LocalDate date, LocalTime startTime, LocalTime endTime){
+        for(Consultation consultation : Consultation.getConsultations()){
+            if(consultation.getDoctor().getMedicalLicenceNumber().equals(doctorId)){
+                if(consultation.getDate().equals(date)){
+                    if(!(startTime.isAfter(consultation.getEndTime())) || !(endTime.isBefore(consultation.getEndTime()))){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
 }
